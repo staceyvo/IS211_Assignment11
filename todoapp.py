fcbf6e3701b2b9dc93bdf66c27eaaad148257ec0
staceyvo <staceyvo@gmail.com>
2018-05-07 21:32:25 -0400

    completed part 2

--- todoapp.py
+++ todoapp.py
@@ -1,33 +1,29 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from wtforms import Form, validators, StringField, SelectField
 
 app = Flask(__name__)
 
+to_do = []
 
-to_do = [
-    {'name': 'order supplies',
-     'email': 'stacey@me.com',
-     'priority': 'medium'
-     },
-    {'name': 'feed the cat',
-     'email': 'chris@you.com',
-     'priority': 'high'
-     },
-    {'name': 'water the plants',
-     'email': 'juan@juan.net',
-     'priority': 'low'
-     }
-]
 
 class CreateTaskForm(Form):
     task = StringField(u'Task', [validators.required(), validators.length(max=200)])
     email = StringField(u'Email Address', [validators.required(), validators.length(max=200)])
-    priority = SelectField(u'Priority', choices=[('low','low'), ('medium', 'medium'), ('high', 'high')], validators=[validators.required()])
+    priority = SelectField(u'Priority', choices=[('low', 'low'), ('medium', 'medium'), ('high', 'high')],
+                           validators=[validators.required()])
 
 
-@app.route('/')
+@app.route('/', methods=['POST', 'GET'])
 def to_do_tasks():
+    if request.method == 'POST':
+        task2 = {
+            'task': request.form['task'],
+            'email': request.form['email'],
+            'priority': request.form['priority']
+        }
+        to_do.append(task2)
     return render_template('task_template.html', data=to_do, my_form=CreateTaskForm())
+
 
 @app.route('/old')
 def display_tasks():
