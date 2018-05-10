4d5f6e4bdf9a2010ef27afd89aec2956e34d23e7
staceyvo <staceyvo@gmail.com>
2018-05-10 07:47:39 -0400

    completed assignment

--- todoapp.py
+++ todoapp.py
@@ -7,6 +7,7 @@ to_do = []
 
 
 class CreateTaskForm(Form):
+    # create a to do list with task, email, and a priority drop down menu
     task = StringField(u'Task', [validators.required(), validators.length(max=200)])
     email = StringField(u'Email Address', [validators.required(), validators.length(max=200)])
     priority = SelectField(u'Priority', choices=[('low', 'low'), ('medium', 'medium'), ('high', 'high')],
@@ -14,6 +15,7 @@ class CreateTaskForm(Form):
 
 @app.route('/clear')
 def clear_list():
+    # create a function to clear the to do list when session ends
     del to_do[:]
     return redirect(url_for('display_tasks'))
 
@@ -21,7 +23,9 @@ def clear_list():
 
 @app.route('/submit', methods=['POST', 'GET'])
 def to_do_tasks():
+    # accept tasks with an email and chosen priority
     if request.method == 'POST':
+        # validate entries and add to list
         if CreateTaskForm(request.form).validate():
             task2 = {
                 'task': request.form['task'],
@@ -29,13 +33,16 @@ def to_do_tasks():
                 'priority': request.form['priority']
             }
             to_do.append(task2)
+            # redirect to display after task entered
         return redirect(url_for('display_tasks'))
+    # return to do template to enter tasks
     return render_template('to_do_template.html', data=to_do, my_form=CreateTaskForm())
 
 
 
 @app.route('/')
 def display_tasks():
+    # display tasks
     return render_template('task_template.html', data=to_do)
 
 
